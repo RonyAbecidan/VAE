db4d4a3f4ca55e1c921f732871caf068f8bb634d
Rony Abecidan <ronyabecidan@msn.com>
2021-01-11 16:27:40 +0100

    [Update] : Updating the code giving documentations and redefining some variables

--- Experiments/MVAE.py
+++ Experiments/MVAE.py
@@ -10,21 +10,31 @@ from collections import OrderedDict
 import matplotlib.pyplot as plt
 import warnings
 import pytorch_lightning as pl
-import h5py as h5
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
+
+warnings.simplefilter("ignore")
 
 # Build Encoder
 class Encoder(pl.LightningModule):
 
     def __init__(self,latent_dim,im_size=32,in_channel=3,hiddens=[128,256,512]):
         super(Encoder, self).__init__()
+        '''
+        @latent_dim : The dimension of the latent space 
+        @im_size : The height/width of the squared image considered
+        @in_channel : Number of channels for the input images (ex : 3 channels for RGB images)
+        @hiddens : A list containing the respectives sizes of the hidden spaces involved in the neural network.
+        
+        Architecture based on 2D convolutions
+        '''
 
         self.latent_dim = latent_dim
         self.im_size=im_size
         self.in_channel=in_channel
         self.hiddens=hiddens
+        
         self.modules=[nn.Conv2d(self.in_channel,hiddens[0], kernel_size=3, stride=2, padding=1),  # hiddens[0]*im_size/2xim_size/2
                       nn.ReLU(),
                       nn.BatchNorm2d(hiddens[0])]
@@ -42,20 +52,19 @@ class Encoder(pl.LightningModule):
 
         self.encode=nn.Sequential(*self.modules)
 
+        #output : the mean and the variance of the distribution of q(z|x)
         self.fc_mu = nn.Linear(self.hiddens[-1] * final_size * final_size, self.latent_dim)
         self.fc_var = nn.Linear(self.hiddens[-1] * final_size * final_size, self.latent_dim)
 
     def forward(self, x):
         """
-        Encodes the input by passing through the encoder network
-        and returns the latent codes.
-        :param input: (Tensor) Input tensor to encoder [N x C x H x W]
+        Encodes the input by passing through the encoder network and returns the latent codes.
+        :param input: (Tensor) Input tensor to encoder [Batch_size x in_channel x H x W]
         :return: (Tensor) List of latent codes
         """
         result = self.encode(x)
 
-        # Split the result into mu and var components
-        # of the latent Gaussian distribution
+        # Split the result into mu and var components of the latent Gaussian distribution
         mu = self.fc_mu(result)
         log_var = self.fc_var(result)
 
@@ -67,6 +76,14 @@ class Decoder_MLP(pl.LightningModule):
 
     def __init__(self, latent_dim=4,in_channel=3,im_size=64,hiddens=[256,512]):
         super(Decoder_MLP, self).__init__()
+        '''
+        @latent_dim : The dimension of the latent space 
+        @in_channel : Number of channels for the input images (ex : 3 channels for RGB images)
+        @im_size : The height/width of the squared image considered
+        @hiddens : A list containing the respectives sizes of the hidden spaces involved in the neural network.
+        
+        Architecture based on Linear layers
+        '''
 
         self.latent_dim = latent_dim
         self.im_size = im_size
@@ -86,10 +103,9 @@ class Decoder_MLP(pl.LightningModule):
 
     def forward(self, z):
         """
-        Maps the given latent codes
-        onto the image space.
-        :param z: (Tensor) [B x D]
-        :return: (Tensor) [B x C x H x W]
+         Maps the given latent codes onto the image space.
+        :param z: (Tensor) [Batch_size x latent_dim]
+        :return: (Tensor) [Batch_size x in_channel x H x W]
         """
         z=z.view(-1,self.latent_dim)
         for layer in self.decode:
@@ -105,6 +121,16 @@ class Decoder_Conv(pl.LightningModule):
 
     def __init__(self, latent_dim=4, in_channel=3, im_size=64, hiddens=[512,256,128,64],init=4):
         super(Decoder_Conv, self).__init__()
+        '''
+            
+        @latent_dim : The dimension of the latent space 
+        @in_channel : Number of channels for the input images (ex : 3 channels for RGB images)
+        @im_size : The height/width of the squared image considered
+        @hiddens : A list containing the respectives sizes of the hidden spaces involved in the neural network.
+        
+        Architecture based on 2D Deconvolutions
+        
+        '''
 
         assert (2**(len(hiddens))*init==im_size),"Take care about the architecture of your decoder, there is something wrong"
 
@@ -129,10 +155,9 @@ class Decoder_Conv(pl.LightningModule):
 
     def forward(self, z):
         """
-        Maps the given latent codes
-        onto the image space.
-        :param z: (Tensor) [B x D]
-        :return: (Tensor) [B x C x H x W]
+         Maps the given latent codes onto the image space.
+        :param z: (Tensor) [Batch_size x latent_dim]
+        :return: (Tensor) [Batch_size x in_channel x H x W]
         """
 
         z = z.view(-1,self.latent_dim,1,1)
@@ -147,6 +172,16 @@ class Decoder_Linear_Conv(pl.LightningModule):
 
     def __init__(self, latent_dim=4, in_channel=3, im_size=64, hiddens=[512,256,128,64],init=4):
         super(Decoder_Linear_Conv, self).__init__()
+        '''
+            
+        @latent_dim : The dimension of the latent space 
+        @in_channel : Number of channels for the input images (ex : 3 channels for RGB images)
+        @im_size : The height/width of the squared image considered
+        @hiddens : A list containing the respectives sizes of the hidden spaces involved in the neural network.
+        
+        Architecture based on a Linear Layer and 2D Deconvolutions
+        
+        '''
 
         assert (2**(len(hiddens))*init==im_size),"Take care about the architecture of your decoder, there is something wrong"
 
@@ -170,10 +205,9 @@ class Decoder_Linear_Conv(pl.LightningModule):
 
     def forward(self, z):
         """
-        Maps the given latent codes
-        onto the image space.
-        :param z: (Tensor) [B x D]
-        :return: (Tensor) [B x C x H x W]
+        Maps the given latent codes onto the image space.
+        :param z: (Tensor) [Batch_size x latent_dim]
+        :return: (Tensor) [Batch_size x in_channel x H x W]
         """
 
         z = z.view(-1,self.latent_dim)
@@ -186,17 +220,6 @@ class Decoder_Linear_Conv(pl.LightningModule):
         return z
 
 
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 1)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 0, 1)
-        nn.init.constant_(m.bias.data, 0)
-    elif classname.find('Linear') != -1:
-        nn.init.normal_(m.weight.data, 0, 1)
-        nn.init.constant_(m.bias.data, 0)
-
 # Decoders=nn.ModuleList([Decoder_MLP(latent_dim=100, in_channel=1, im_size=32, hiddens=[256,512]),
 #           Decoder_Conv(latent_dim=100, in_channel=1, im_size=32, hiddens=[512,256,128],init=4),
 #           Decoder_Linear_Conv(latent_dim=100, in_channel=1, im_size=32, hiddens=[512,256,128,64],init=2)])
@@ -204,17 +227,32 @@ def weights_init(m):
 class MabVAE(pl.LightningModule):
     def __init__(self,train_loader,decoders,eps=0.1,i=0):
         super(MabVAE, self).__init__()
+        '''
+            
+        @train_loader : A pytorch dataloader containing all the images of the training set 
+        @decoders : A list of proposals for decoders
+        @epsilon : parameter of the epsilon-greedy strategy for solving the MAB problem
+        
+        This class implements the MAB-VAE model envisioned in the report using the famous epsilon-greedy strategy 
+        (See Sutton et al,Reinforcement Learning: An Introduction)
+        
+        '''
+        
         self.eps=eps
         self.decoders=decoders
         self.nb_decoders=len(decoders)
+        # the latent dims, im_size and in_channels are deduced from the decoders
         self.latent_dim=self.decoders[0].latent_dim
         self.im_size = self.decoders[0].im_size
         self.in_channel=self.decoders[0].in_channel
         self.encoder = Encoder(latent_dim=self.latent_dim,in_channel=decoders[0].in_channel,im_size=self.im_size)
+        #tensor storing information about the number of times a decoder is chosen and the reward associated
         self.history=torch.zeros(self.nb_decoders)
         self.NbDraws=torch.zeros(self.nb_decoders)
+        #list for storing the reconstruction losses obtained through the time following our strategy
         self.strategy_path=[]
         self.train_loader = train_loader
+        #list for storing the best rewards obtainable at each round 
         self.best_rewards=[]
         self.latent = torch.randn(64, self.latent_dim, 1, 1)
         self.i=i #counter of epochs
@@ -222,11 +260,10 @@ class MabVAE(pl.LightningModule):
 
     def reparameterize(self, mu, logvar):
         """
-        Reparameterization trick enabling to sample from N(mu, var) using
-        N(0,1).
-        :param mu: (Tensor) Mean of the latent Gaussian [B x D]
-        :param logvar: (Tensor) Standard deviation of the latent Gaussian [B x D]
-        :return: (Tensor) [B x D]
+        Reparameterization trick enabling to sample from N(mu, var) using N(0,1).
+        :param mu: (Tensor) Mean of the latent Gaussian [batch_size x latent_dim]
+        :param logvar: (Tensor) Standard deviation of the latent Gaussian [batch_size x latent_dim]
+        :return: (Tensor) [batch_size x latent_dim]
         """
         std = torch.exp(0.5 * logvar).type_as(mu)
         eps = torch.randn_like(std).type_as(mu)
@@ -236,19 +273,25 @@ class MabVAE(pl.LightningModule):
         """
         Computes the VAE loss function.
         KL(N(\mu, \sigma), N(0, 1)) = \log \frac{1}{\sigma} + \frac{\sigma^2 + \mu^2}{2} - \frac{1}{2}
+        Reconstruction_Loss = BCE
         """
         recon_loss = F.binary_cross_entropy(recons.view(-1, self.im_size, self.im_size), input.view(-1, self.im_size, self.im_size), reduction='sum')
         
-        # minimize kl = maximize -kl
+        
         weight = (len(self.train_loader.dataset) / (recons.size(0)))
+        #maximize -kl = minimize kl 
         kld_loss = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
-
+ 
         loss = weight * (recon_loss + kld_loss)
 
         return {'total_loss': loss, 'Reconstruction_Loss': weight * recon_loss, 'KL': weight * kld_loss}
         # Optimizers
 
     def configure_optimizers(self):
+        '''
+        Configuration of the optimizers used for the encoder and the decoders
+        '''
+        
         opt_list=[]
         for decoder in self.decoders:
             optimizer = torch.optim.Adam(decoder.parameters())
@@ -265,48 +308,42 @@ class MabVAE(pl.LightningModule):
     def forward(self, x):
         return self.encoder(x)
 
-    # def display(self):
-    #     fake = self.decoder(self.latent).detach()
-    #     plt.figure(figsize=(10, 10))
-    #     plt.imshow(np.transpose(utils.make_grid(fake, padding=2, normalize=True).cpu(), (1, 2, 0)))
-    #     plt.savefig(f'VAE_current_result.png')
-    #     print('OK')
-    #     plt.close('all')
-
     # Training Loop
     def training_step(self, batch, batch_idx, optimizer_idx):
         # batch returns x and y tensors
         real_images, _ = batch
        
-        #for the gpu
+        #enable to activate the computations on the gpu
         if self.t==1:
             self.latent = self.latent.type_as(real_images[0])
             self.history=self.history.type_as(real_images[0])
             self.NbDraws=self.NbDraws.type_as(real_images[0])
 
         #encoding
-        
         mu, log_var = self.encoder(real_images)
         mu=mu.type_as(real_images[0])
         log_var=log_var.type_as(real_images[0])
+        
+        #sampling of the latent variable
         z = self.reparameterize(mu, log_var).type_as(real_images[0])
-        # print('OK1')
+       
         with torch.no_grad():
-            best_reward=10**(8)
+            best_reward=-10**(10)
             
+            #Computation of the best reward achievable at time t
             for decoder in self.decoders:
-                
                 recons=decoder(z).type_as(real_images[0])
-                reward=self.loss_function(recons, real_images, mu, log_var)['Reconstruction_Loss']
-                if reward<best_reward:
+                reward=-self.loss_function(recons, real_images, mu, log_var)['Reconstruction_Loss']
+                if reward>best_reward:
                     best_reward=reward
 
             self.best_rewards.append(best_reward)
         
-        #initialization
-        if self.t<self.nb_decoders:
-            id_to_choose=self.t
+        #initialization of all the decoders
+        if self.t<100*(self.nb_decoders):
+            id_to_choose=np.random.randint(self.nb_decoders)
             self.NbDraws[id_to_choose]+=1
+            
         #eps-greedy behaviour
         else:
             u=np.random.random()
@@ -314,18 +351,19 @@ class MabVAE(pl.LightningModule):
                 id_to_choose=np.random.randint(self.nb_decoders)
                 self.NbDraws[id_to_choose] += 1
             else:
-                average_previous_rewards=self.history/self.NbDraws
-                id_to_choose=torch.argmin(average_previous_rewards).item()
+                average_previous_rewards=(self.history/self.NbDraws)
+                id_to_choose=torch.argmax(average_previous_rewards).item()
                 self.NbDraws[id_to_choose] += 1
 
         # Encoder-Decoder
         recons = self.decoders[id_to_choose](z).type_as(real_images[0])
         step_dict = self.loss_function(recons, real_images, mu, log_var)
         
-        self.history[id_to_choose]+=step_dict['Reconstruction_Loss']
-        self.strategy_path.append(step_dict['Reconstruction_Loss'])
+        self.history[id_to_choose]-=step_dict['Reconstruction_Loss']
+        self.strategy_path.append(-step_dict['Reconstruction_Loss'])
         total_loss = step_dict['total_loss']
         
+        # periodic save in order to monitor the evolution of the training for the decoders
         if self.t % 100 == 0:
             fake = self.decoders[id_to_choose](self.latent).detach()
             plt.figure(figsize=(10, 10))
@@ -335,6 +373,7 @@ class MabVAE(pl.LightningModule):
 
         self.t += 1
 
+        #We update only the parameters of the chosen decoder
         if (optimizer_idx in [id_to_choose,self.nb_decoders]):
             # {'loss': loss, 'Reconstruction_Loss':recons_loss, 'KLD':-kld_loss}
             output = OrderedDict({
@@ -344,13 +383,3 @@ class MabVAE(pl.LightningModule):
             })
 
             return output
-
-        
-    # # calls after every epoch ends
-    # def on_epoch_end(self):
-    #     fake = self.decoder(self.latent).detach()
-    #     plt.figure(figsize=(10, 10))
-    #     plt.imshow(np.transpose(utils.make_grid(fake, padding=2, normalize=True).cpu(), (1, 2, 0)))
-    #     plt.savefig(f'VAE_epoch_fashion_{self.i}.png')
-    #     plt.close('all')
-    #     self.i += 1
